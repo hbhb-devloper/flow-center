0675099343738e8d5561b40d21e73ad60fbffc75
wangxiaogang <15151269@163.com>
2020-11-17 09:14:40 +0800

    修改流程中心api

--- src/main/java/com/hbhb/cw/flowcenter/web/controller/FlowRoleController.java
+++ src/main/java/com/hbhb/cw/flowcenter/web/controller/FlowRoleController.java
@@ -1,7 +1,7 @@
 package com.hbhb.cw.flowcenter.web.controller;
 
+import com.hbhb.cw.flowcenter.api.FlowApi;
 import com.hbhb.cw.flowcenter.service.RoleUserService;
-import com.hbhb.cw.flow.manage.api.FlowApi;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
