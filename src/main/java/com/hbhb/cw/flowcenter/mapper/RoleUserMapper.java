4c5fd2383a85378e3a4eb094e0d26382747934a7
wangxiaogang <15151269@163.com>
2020-11-17 09:17:12 +0800

    修改流程中心api [ci skip]

--- src/main/java/com/hbhb/cw/flowcenter/mapper/RoleUserMapper.java
+++ src/main/java/com/hbhb/cw/flowcenter/mapper/RoleUserMapper.java
@@ -1,6 +1,7 @@
 package com.hbhb.cw.flowcenter.mapper;
 
-import com.hbhb.cw.flow.manage.model.FlowRoleUser;
+
+import com.hbhb.cw.flowcenter.model.FlowRoleUser;
 import com.hbhb.web.beetlsql.BaseMapper;
 
 import java.util.List;
